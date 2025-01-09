cf509e36c725ec4032f04fba08e1de24a38b32d3
Mia <miaelanderson@gmail.com>
2025-01-09 09:41:03 -0600

    Initial Commit

---
+++ Country Flag Game/Country_Flag_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Country_Flag_GameApp.swift
+//  Country Flag Game
+//
+//  Created by Mia Anderson on 1/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Country_Flag_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
