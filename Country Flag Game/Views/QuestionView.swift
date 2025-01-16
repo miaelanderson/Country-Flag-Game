e2dd981bde5e62e28ade1bd43344cd3c2a456cfe
Mia <miaelanderson@gmail.com>
2025-01-16 09:52:44 -0600

    fixing end page

--- Country Flag Game/Views/QuestionView.swift
+++ Country Flag Game/Views/QuestionView.swift
@@ -14,12 +14,30 @@ struct QuestionView: View {
             if gameManager.playingGame {
                 HStack {
                     Text("Country Flag Game")
-                        .foregroundColor(.yellow)
                         .fontWeight(.heavy)
+                        .padding()
                     Spacer()
                     Text("\(gameManager.index) out of \(gameManager.questions.count)")
+                        .padding()
                 }
-                    .foregroundColor(.yellow)
+                ProgressBar(progress: gameManager.progress)
+                VStack(spacing: 10) {
+                    Text("Which country's flag is this?")
+                    Image(gameManager.country)
+                        .resizable()
+                        .frame(width: 300, height: 200)
+                    ForEach(gameManager.answerChoices) { answer in
+                        AnswerRow(answer: answer)
+                            .environmentObject(gameManager)
+                    }
+                }
+                Button {
+                    gameManager.goToNextQuestion()
+                } label: {
+                    CustomButton(text: "Next", background: gameManager.answerSelected ? .yellow : .gray)
+                }
+                .disabled(!gameManager.answerSelected)
+                Spacer()
             }
             else {
                 Text("Country Flag Game")
@@ -33,27 +51,11 @@ struct QuestionView: View {
                     CustomButton(text: "Play Again")
                 }
             }
-            ProgressBar(progress: gameManager.progress)
-            VStack(spacing: 10) {
-                Text("Which country's flag is this?")
-                Image(gameManager.country)
-                    .resizable()
-                    .frame(width: 300, height: 200)
-                ForEach(gameManager.answerChoices) { answer in
-                    AnswerRow(answer: answer)
-                        .environmentObject(gameManager)
-                }
-            }
-            Button {
-                gameManager.goToNextQuestion()
-            } label: {
-                CustomButton(text: "Next", background: gameManager.answerSelected ? .yellow : .gray)
-            }
-            .disabled(!gameManager.answerSelected)
-            Spacer()
         }
-        .padding()
+        .foregroundColor(.yellow)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(.cyan)
+        .navigationBarBackButtonHidden(true)
     }
 }
 
